843c916ccc2e45a089a308c1d53c78629c4f1275
Rodrigo Gomes <mariadefatima071965@gmail.com>
2023-10-19 15:29:12 -0300

    feat: add auth handler to header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+"use client";
+
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
+  const { data } = useSession();
+
+  return <div></div>;
 }
