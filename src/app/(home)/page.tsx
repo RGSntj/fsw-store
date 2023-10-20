7ac0f9e84ade8e155fdcce15e18013868dd8d4f3
Rodrigo Gomes <mariadefatima071965@gmail.com>
2023-10-20 14:49:23 -0300

    feat: add Categories and CategoriesItem component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,7 @@
 
 import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { Categories } from "./components/categories";
 
 export default function Home() {
   const { data } = useSession();
@@ -16,6 +17,10 @@ export default function Home() {
         sizes="100vw"
         alt="Até 55% de desconto esse mês!"
       />
+
+      <div className="mt-8">
+        <Categories />
+      </div>
     </div>
   );
 }
