f622b8e0bea39e323cc2a1a439d0da76488d09aa
Rodrigo Gomes <mariadefatima071965@gmail.com>
2023-10-21 16:08:11 -0300

    feat: add ProductItem and ProductList components

---
+++ src/app/(home)/components/product-list.tsx
@@ -0,0 +1,22 @@
+import { ProductItem } from "@/components/ui/product-item";
+import { computeProductTotalPrice } from "@/helpers/product";
+import { Product } from "@prisma/client";
+
+interface ProductProps {
+  products: Product[];
+}
+
+export const ProductList = ({ products }: ProductProps) => {
+  return (
+    <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+      {products.map((product) => {
+        return (
+          <ProductItem
+            key={product.id}
+            product={computeProductTotalPrice(product)}
+          />
+        );
+      })}
+    </div>
+  );
+};
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,25 +1,29 @@
-"use client";
-
-import { useSession } from "next-auth/react";
 import Image from "next/image";
 import { Categories } from "./components/categories";
+import { ProductUseCase } from "@/use-cases/products";
+import { ProductList } from "./components/product-list";
 
-export default function Home() {
-  const { data } = useSession();
+export default async function Home() {
+  const productUseCase = new ProductUseCase();
+  const productsWithDeal = await productUseCase.allProductWithDeals();
 
   return (
-    <div className="p-5">
+    <div className="">
       <Image
         src="/banner-home-01.png"
         height={0}
         width={0}
-        className="h-auto w-full"
+        className="h-auto w-full px-5"
         sizes="100vw"
         alt="Até 55% de desconto esse mês!"
       />
 
-      <div className="mt-8">
+      <div className="mt-8 px-5">
         <Categories />
+      </div>
+
+      <div className="mt-8">
+        <ProductList products={productsWithDeal} />
       </div>
     </div>
   );
